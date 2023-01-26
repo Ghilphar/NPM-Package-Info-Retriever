c9885bf93516412c32f676b93190a63905bf2acc
FGaribot <felipegaribotti@gmail.com>
2023-01-26 11:52:57 +0100

    Finish Project

--- README.md
+++ README.md
@@ -1,29 +1,39 @@
 # README
 
+This script allows you to extract data from npm packages and save it to a CSV file.
+
 This project is a Python script that utilizes the npm registry API (https://github.com/npm/registry/blob/master/docs/REGISTRY-API.md#objects) to retrieve information about a specified npm package. The script specifically retrieves the total number of downloads for the package since its creation.
 
 The retrieved information is then organized and saved into a CSV file, which is encoded in ISO-8859-1 and uses the "|" character as a delimiter.
 
 ## Usage
-To use the script, navigate to the directory where the script is located and run the following command:
 
-```
-python script.py <package_name>
-```
+1. Run the script using the command `python3 script.py package1 package2 package3` (replace package1, package2, package3 with the names of the packages you want to extract data from).
 
-Where `<package_name>` is the name of the npm package you wish to retrieve information for.
+2. The script will extract the following data for each package:
+    - name
+    - description
+    - license
+    - latest version
+    - author's name
+    - author's email
+    - total downloads from 2010-01-13 to today
 
-Please make sure that you have python3 installed.
+3. The extracted data will be saved to two CSV files:
+    - packageData.csv: contains data for each package in a separate row
+    - packageData2.csv: contains all the data for all the packages in the same file
+
+4. The script uses the npm API to get package data and the requests library to handle API calls. It also uses the csv library to write data to the CSV files and sys library to get the command line argument.
+
+5. Each time the script runs, it will overwrite the existing CSV files.
 
 ## Dependencies
 This script utilizes the following Python packages:
 - requests
-- pandas
 
-You can install them using pip:
-
+You will need to have these packages installed before running the script. You can install them using pip:
 ```
-pip install requests pandas
+pip install requests
 ```
 
 ## Contribution
--- infoRetriever.py
+++ infoRetriever.py
@@ -1,44 +1,70 @@
 import requests
-import pandas as pd
-import json
 import csv
+from datetime import datetime
+import sys
 
+packageNumber = 1
+numberOfPackage = len(sys.argv)
 
-package_name = "express"
-url = f"https://registry.npmjs.org/{package_name}"
-response = requests.get(url)
+#Create the list for the csv2
+MyList = []
 
-data = response.json()
+# Create the fieldnames and the csv
+fieldnames = ["name", "downloads","description", "license", "latest_version", "author_name", "author_email"]
+with open("packageData.csv", 'w', newline='', encoding='ISO-8859-1') as csvfile:
+    csvwriter = csv.DictWriter(csvfile, fieldnames=fieldnames, delimiter='|')
+    csvwriter.writeheader()
 
-packageData = {}
-packageData["name"] = data["name"]
-packageData["description"] = data["description"]
-packageData["license"] = data["license"]
-packageData["latest_version"] = data["dist-tags"]["latest"]
-packageData["author_name"] = data["versions"][data["dist-tags"]["latest"]]["author"]["name"]
-packageData["author_email"] = data["versions"][data["dist-tags"]["latest"]]["author"]["email"]
+while (packageNumber < numberOfPackage):
+    package_name = sys.argv[packageNumber]
+    packageNumber += 1
+    #Get all the downloads of a package
+    dateCreationOfNpm = "2010-01-13"
+    dateStart = datetime.today().strftime('%Y-%m-%d')
+    dateEnd = dateCreationOfNpm
+    downloads = 0
 
-# Create the DataFrame
-df = pd.DataFrame(packageData, index=[0])
+    while (dateEnd != dateStart):
+        url = f"https://api.npmjs.org/downloads/point/{dateCreationOfNpm}:{dateStart}/{package_name}"
+        responseDownloads = requests.get(url)
+        dataDownloads = responseDownloads.json()
+        print(dataDownloads)
+        downloads += dataDownloads["downloads"]
+        dateStart = dataDownloads["start"]
+        dateEnd = dataDownloads["end"]
 
-# Write the DataFrame to a CSV file
-df.to_csv("packageData.csv", sep='|', encoding='ISO-8859-1')
+    #Get the info of the package
+    url = f"https://registry.npmjs.org/{package_name}"
+    response = requests.get(url)
+    data = response.json()
 
-#with open("packageData.csv", 'w', newline='') as file:
-#    writer = csv.DictWriter(file, fieldnames=packageData.keys())
-#    writer.writeheader()
-#    writer.writerow(packageData)
+    #Create the dictionary for the csv
+    packageData = {}
+    author = data["versions"][data["dist-tags"]["latest"]].get("author", {})
+    packageData["name"] = data["name"]
+    packageData["description"] = data["description"]
+    packageData["license"] = data["license"]
+    packageData["latest_version"] = data["dist-tags"]["latest"]
+    packageData["author_name"] = author.get("name", "N/A")
+    packageData["author_email"] = author.get("email", "N/A")
+    packageData["downloads"] = downloads
 
+    print(packageData)
 
-#with open("package_data.json", "w") as outfile:
-#    json.dump(packageData, outfile)
+    #Add the dictionary to the list of the csv2
+    MyList.append(packageData)
+    #Create a list to add it to the csv1
+    rows = [packageData]
 
-#print(data)
+    # Open the file and write the data using the csv module
+    with open("packageData.csv", 'a', newline='', encoding='ISO-8859-1') as csvfile:
+        csvwriter = csv.DictWriter(csvfile, fieldnames=fieldnames, delimiter='|')
+        csvwriter.writerows(rows)
 
-#json_string = json.dumps(packageData)
+with open("packageData2.csv", 'w', newline='', encoding='ISO-8859-1') as csvfile:
+    csvwriter = csv.DictWriter(csvfile, fieldnames=fieldnames, delimiter='|')
+    csvwriter.writeheader()
+    csvwriter.writerows(MyList)
 
-
-#data_frame = pd.DataFrame.from_dict(packageData)
-#print(data_frame)
-
-#data_frame.to_csv("package_data.csv", sep='|', encoding='ISO-8859-1')
+#This is the end of the project We show 2 ways of create a csv file with the data we extracted.
+#Author: FGaribot
